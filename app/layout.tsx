c9f76425b021ce049bd01c0c633628c62bb2e573
Cyanidezz <42004039+Cyanidezz@users.noreply.github.com>
2026-09-16 11:25:07 +0700

    Refresh Wingpro visual theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
   },
 };
 
-export const viewport: Viewport = {width: "device-width", initialScale: 1, viewportFit: "cover", themeColor: "#087fac"};
+export const viewport: Viewport = {width: "device-width", initialScale: 1, viewportFit: "cover", themeColor: "#063f5b"};
 
 export default function RootLayout({
   children,
